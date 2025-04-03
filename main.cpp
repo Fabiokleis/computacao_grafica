d70c768284874e29ef83822172a233389727df11
fabiokleis <fabiohkrc@gmail.com>
2025-04-03 18:57:36 -0300

    square green to black and black to green

--- main.cpp
+++ main.cpp
@@ -11,6 +11,26 @@
 
 #define MOUSE_ICON_FILE "mouse_icon.png"
 
+
+const static char *vertex_shader_source = "#version 330 core\n"
+  "layout (location = 0) in vec3 pos;\n"
+  "layout (location = 1) in vec3 v_color;\n"
+  "out vec3 color;\n"
+  "void main()\n"
+  "{\n"
+  "   gl_Position = vec4(pos, 1.0);\n"
+      "color = v_color\n;"
+  "}\0";
+const static char *fragment_shader_source = "#version 330 core\n"
+  "in vec3 color;\n"
+  "out vec4 FragColor;\n"
+  "uniform vec4 ourColor;\n"
+  "void main()\n"
+  "{\n"
+  "   FragColor = vec4(ourColor);\n"
+  "}\n\0";
+
+
 typedef struct {
   double x, y;
 } Vec2;
@@ -39,6 +59,52 @@ Vec2 get_mouse_pos(GLFWwindow *window) {
 void resize_callback(GLFWwindow* window, int width, int height) {
   glfwGetWindowSize(window, &width, &height);
   glViewport(0, 0, width, height);
+}
+
+
+int compile_shaders(uint32_t *shader_program) {
+
+  // vertex shader
+  unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+  glCompileShader(vertex_shader);
+  // check for shader compile errors
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success)
+    {
+      glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+      return -1;
+    }
+  // fragment shader
+  uint32_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+  glCompileShader(fragment_shader);
+  // check for shader compile errors
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success)
+    {
+      glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+      return -1;
+    }
+  // link shaders
+  *shader_program = glCreateProgram();
+  glAttachShader(*shader_program, vertex_shader);
+  glAttachShader(*shader_program, fragment_shader);
+  glLinkProgram(*shader_program);
+  // check for linking errors
+  glGetProgramiv(*shader_program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(*shader_program, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    return -1;
+  }
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+  return 0;
 }
 
 void loop(GLFWwindow *window) {
@@ -79,9 +145,47 @@ void loop(GLFWwindow *window) {
   
   glfwSetCursor(window, cursor);
 
-  
   bool quit = false;
   glfwSetFramebufferSizeCallback(window, resize_callback);
+
+
+  uint32_t program;
+  int error = compile_shaders(&program);
+  if (error != 0) exit(1);
+  
+  // ------------------------------------------------------------------
+  float vertices[] = {
+    0.5f,  0.5f, 0.0f, 0.5f, 0.1, 0.8, // x y z rgb
+    0.5f, -0.5f, 0.0f, 0.5f, 0.1, 0.8,
+    -0.5f,  -0.5f, 0.0f, 0.5f, 0.1, 0.8,
+    -0.5f,  0.5f, 0.0f, 0.5f, 0.1, 0.8,
+  };
+  uint32_t indices[] = {  // note that we start from 0!
+    0, 1, 3,  // first Triangle
+    1, 2, 3   // second Triangle
+  };
+
+  uint32_t VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
 
   while (!quit) {
     glfwPollEvents();
@@ -90,33 +194,31 @@ void loop(GLFWwindow *window) {
     //glfwSwapInterval(1);
     glfwGetWindowSize(window, &width, &height);
     mouse_pos = get_mouse_pos(window);
-    // std::cout << "width: " << width << std::endl;
-    // std::cout << "height: " << height << std::endl;
-    // std::cout << "x: " << mouse_pos.x << std::endl;
-    // std::cout << "y: " << mouse_pos.y << std::endl;
 
     if (is_mouse_button_pressed(window, GLFW_MOUSE_BUTTON_LEFT)) {
       glClearColor(0.99, 0.3, 0.3, 1.0);
-
-      /* Clears color buffer to the RGBA defined values. */
-      glClear(GL_COLOR_BUFFER_BIT);
-
-      /* Demand to draw to the window.*/
-      glfwSwapBuffers(window);      
     } else {
       // draw
-      {
-	glClearColor(0.2, 0.3, 0.3, 1.0);
-
-	/* Clears color buffer to the RGBA defined values. */
-	glClear(GL_COLOR_BUFFER_BIT);
-      
-	/* Demand to draw to the window.*/
-	glfwSwapBuffers(window);
-      }
-    
-
+      glClearColor(0.2, 0.3, 0.3, 1.0);
     }
+
+    /* Clears color buffer to the RGBA defined values. */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    float timeValue = glfwGetTime();
+    float greenValue = (sin(timeValue * 2.0f) / 2.0f) + 0.5f;
+    int vertexColorLocation = glGetUniformLocation(program, "ourColor");
+    glUseProgram(program);
+    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+    
+    /* Demand to draw to the window.*/
+    //glUseProgram(program);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    glfwSwapBuffers(window);
   }
   glfwDestroyCursor(cursor);
 }
